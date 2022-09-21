17db08c17ddcb43d0e2363b57d8eaf35687ff35c
Luis Coelho <mancoeluis@gmail.com>
2022-09-21 20:49:56 -0300

    🗃️ make birthdate optional

---
+++ prisma/migrations/20220921190820_some_changes/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE `users` MODIFY `birthdate` DATETIME(3) NULL;
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,17 @@
-console.log('Testing tsx')
+import { PrismaClient } from '@prisma/client'
+const prisma = new PrismaClient({log: ["query"]})
+
+async function main() {
+  await prisma.user.deleteMany()
+
+  const user = await prisma.user.create({data: {
+    name: "Luis",
+    cpf: "111.222.333.-00",
+  },
+  select: {
+    name: true,
+  },
+  })
+}
+
+main()
